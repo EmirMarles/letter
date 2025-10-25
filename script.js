a109597979c451011d9f25650b6223625cc5d712
Emir <emirmarles90@gmail.com>
2025-10-25 20:53:15 +0200

    Remove stamp completely and make entire envelope clickable

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const envelope = document.querySelector(".envelope-wrapper");
-const stamp = document.querySelector(".stamp");
 
-stamp.addEventListener("click", ()=>{
+envelope.addEventListener("click", ()=>{
     envelope.classList.toggle("flap")
 })
